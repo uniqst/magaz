99113e450361c0acfb06bc35cfcdb0bdbe927364
sadsenpai <sad.senpai.2001@gmail.com>
2017-11-06 21:10:11 +0200

    last changes

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,16 +12,12 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        // 'css/site.css',
-        'css/styles.css',
+        'css/site.css',
     ];
     public $js = [
-        'js/libs/jquery.min.js',
-        'js/libs/popper.min.js',
-        'js/libs/bootstrap.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        // 'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapAsset',
     ];
 }
