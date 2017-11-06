39f28609901e1908ceffb15c827815973b4f190b
sadsenpai <sad.senpai.2001@gmail.com>
2017-11-06 20:49:18 +0200

    sass compilation is working now

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,12 +12,16 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        // 'css/site.css',
+        'css/styles.css',
     ];
     public $js = [
+        'js/libs/jquery.min.js',
+        'js/libs/popper.min.js',
+        'js/libs/bootstrap.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        // 'yii\bootstrap\BootstrapAsset',
     ];
 }
