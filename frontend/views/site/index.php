7500257eefcfdcaf6261323a2a04df43c0b7b565
sadsenpai <sad.senpai.2001@gmail.com>
2017-11-07 17:05:51 +0200

    last resond bod is ready

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -7,64 +7,66 @@ $this->title = 'My Yii Application';
 
 <div class="container">
     <div class="row">
-        <div class="col-sm-2">Side bar</div>
-        <div class="col-sm-8">
-            <div class="main-slider-container">
+    <div class="col main-slider-container">
 
-                <h3 class="subheading-main">New Girls</h3>
+        <h3 class="subheading-main">New Girls</h3>
 
-                <div class="new-girls-slider">
+        <div class="new-girls-slider">
 
-                    <div class="slick-carousel">
-                        <div class="slider-item">
-                            <div class="girl-card" style="background-image: url(/img/girl-cards/girl-card-1.jpg)">
-                                <a class="girl-card__name">Katrina</a>
-                            </div>
-                        </div>
-                        <div class="slider-item">
-                            <div class="girl-card" style="background-image: url(/img/girl-cards/girl-card-1.jpg)">
-                                <a class="girl-card__name">Katrina</a>
-                            </div>
-                        </div>
-                        <div class="slider-item">
-                            <div class="girl-card" style="background-image: url(/img/girl-cards/girl-card-1.jpg)">
-                                <a class="girl-card__name">Katrina</a>
-                            </div>
-                        </div>
-                        <div class="slider-item">
-                            <div class="girl-card" style="background-image: url(/img/girl-cards/girl-card-1.jpg)">
-                                <a class="girl-card__name">Katrina</a>
-                            </div>
-                        </div>
-                        <div class="slider-item">
-                            <div class="girl-card" style="background-image: url(/img/girl-cards/girl-card-1.jpg)">
-                                <a class="girl-card__name">Katrina</a>
-                            </div>
-                        </div>
-                        <div class="slider-item">
-                            <div class="girl-card" style="background-image: url(/img/girl-cards/girl-card-1.jpg)">
-                                <a class="girl-card__name">Katrina</a>
-                            </div>
-                        </div>
-                        <div class="slider-item">
-                            <div class="girl-card" style="background-image: url(/img/girl-cards/girl-card-1.jpg)">
-                                <a class="girl-card__name">Katrina</a>
-                            </div>
-                        </div>
+            <div class="slick-carousel">
+                <div class="slider-item">
+                    <div class="girl-card" style="background-image: url(/img/girl-cards/girl-card-1.jpg)">
+                        <a class="girl-card__name" href="#">Katrina</a>
+                    </div>
+                </div>
+                <div class="slider-item">
+                    <div class="girl-card" style="background-image: url(/img/girl-cards/girl-card-1.jpg)">
+                        <a class="girl-card__name" href="#">Katrina</a>
+                    </div>
+                </div>
+                <div class="slider-item">
+                    <div class="girl-card" style="background-image: url(/img/girl-cards/girl-card-1.jpg)">
+                        <a class="girl-card__name" href="#">Katrina</a>
+                    </div>
+                </div>
+                <div class="slider-item">
+                    <div class="girl-card" style="background-image: url(/img/girl-cards/girl-card-1.jpg)">
+                        <a class="girl-card__name" href="#">Katrina</a>
+                    </div>
+                </div>
+                <div class="slider-item">
+                    <div class="girl-card" style="background-image: url(/img/girl-cards/girl-card-1.jpg)">
+                        <a class="girl-card__name" href="#">Katrina</a>
+                    </div>
+                </div>
+                <div class="slider-item">
+                    <div class="girl-card" style="background-image: url(/img/girl-cards/girl-card-1.jpg)">
+                        <a class="girl-card__name" href="#">Katrina</a>
+                    </div>
+                </div>
+                <div class="slider-item">
+                    <div class="girl-card" style="background-image: url(/img/girl-cards/girl-card-1.jpg)">
+                        <a class="girl-card__name">Katrina</a>
                     </div>
                 </div>
             </div>
-
-            <div class="last-responce">
-                <h3 class="subheading-main">Customer response</h3>
-                <blockquote>
-                    <p>Vis primis omnium hendrerit no, ei sumo clita salutatus est. Cu nisl falli ocurreret pri, ne duo aeque accusam salutandi. Vim ex commune definitiones, duo vero audiam alterum cu, has id graeci viderer malorum. Sed epicuri maluisset omittantur in, ornatus fierent dolores mel an. Integre platonem posidonium an his, in duo dicta labores facilisi, ceteros intellegebat vis te.</p>
-                    <cite></cite>
-                </blockquote>
-            </div>
-
         </div>
-        <div class="col-sm-2">Side bar</div>
+    </div>
+    </div>
+    <div class="row">
+        <div class="last-response">
+            <h3 class="subheading-main">Customer response</h3>
+            <div class="iconblock">
+                <a href="#" class="pencil"></a>
+                <a href="#" class="msgbox"></a>
+            </div>
+            <blckquote class="response">
+                <p class="response__quote">Vis primis omnium hendrerit no, ei sumo clita salutatus est. Cu nisl falli ocurreret pri, ne duo aeque accusam salutandi. Vim ex commune definitiones, duo vero audiam alterum cu, has id graeci viderer malorum. Sed epicuri maluisset omittantur in, ornatus fierent dolores mel an. Integre platonem posidonium an his, in duo dicta labores facilisi, ceteros intellegebat vis te.</p>
+                <div class="response__author">
+                    <cite>Mr. Customuer</cite>
+                </div>
+            </blockquote>
+        </div>
     </div>
 </div>
 
