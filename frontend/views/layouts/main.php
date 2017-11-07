65bebdb810e17b1c82ef32c9ad0d638d6446a74a
LtFrager <sadesade2011@gmail.com>
2017-11-07 13:01:18 +0200

    Commit 3

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -44,71 +44,8 @@ AppAsset::register($this);
     </nav>
 
     <div class="container-fluid h-100">
-        <div class="row no-margin h-100">
 
-            <div class="col-2 collapse d-md-flex sidebar-bg pt-2 h-100" id="sidebar">
-
-                <ul class="nav flex-column flex-nowrap" style="width: 100%">
-
-                    <li class="nav-item">
-
-                        <a class="nav-link collapsed" href="#submenu1" data-toggle="collapse" data-target="#submenu1">
-                            Reports
-                            <span class="float-right">
-                                |
-                            </span>
-                        </a>
-
-                        <div class="collapse" id="submenu1">
-
-                            <ul class="flex-column pl-2 nav">
-
-                                <li class="nav-item">
-
-                                    <a class="nav-link collapsed py-0" href="#submenu1sub1" data-toggle="collapse" data-target="#submenu1sub1">
-                                        Customers <span>|</span>
-                                    </a>
-
-                                    <div class="collapse small" id="submenu1sub1" aria-expanded="false">
-
-                                        <ul class="flex-column nav pl-4">
-                                            <li class="nav-item">
-                                                <a class="nav-link p-0" href="">
-                                                    - Daily
-                                                </a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link p-0" href="">
-                                                    - Dashboard
-                                                </a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link p-0" href="">
-                                                    - Charts
-                                                </a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link p-0" href="">
-                                                   - Areas
-                                                </a>
-                                            </li>
-                                        </ul>
-
-                                    </div><!--collapse submenu-->
-
-                                </li><!-- 2 level collapse-->
-
-                            </ul>
-
-                        </div><!--collapse-->
-
-                    </li><!-- 1 level collapse-->
-                    
-                </ul>
-
-            </div><!--sidebar-->
-
-            <div class="col pt-2"><!--site content-->
+            <div ><!--site content-->
                 <?= Breadcrumbs::widget([
                     'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
                 ]) ?>
