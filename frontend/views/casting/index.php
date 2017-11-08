bb3cdafce57af278fc1e7f8ff7607e67e4be228f
sadsenpai <sad.senpai.2001@gmail.com>
2017-11-08 14:55:14 +0200

    inputs is ready, file upload and submit section left

--- frontend/views/casting/index.php
+++ frontend/views/casting/index.php
@@ -1,8 +1,9 @@
 <div>
   <div class="application">
-
-    <h2 class="heading-main">Appliation</h1>
-    <p>You are young and brave? Become one of our escorts!</p>
+    
+    <h2 class="page-name">Appliation</h1>
+    <h1 class="heading-main">Bursa platinum girls. High class escors. Pleasure without limits</h1>
+    <p class="text">You are young and brave? Become one of our escorts!</p>
 
     <form class="main-application-form container-fluid" action="/CastingController.php" method="post" novalidate="novalidate">
       <div class="row">
@@ -12,42 +13,47 @@
         <div class="col-md-6" >
           <input type="text" name="phone" placeholder="your phone number*">
         </div>
-      </div>
-      <div class="row">
         <div class="col-md-6" >
           <input type="text" name="age" placeholder="age*">
         </div>
         <div class="col-md-6" >
           <input type="text" name="weight" placeholder="weight*">
         </div>
-      </div>
-      <div class="row">
         <div class="col-md-6">
           <input type="email" name="email" placeholder="e-mail*">
         </div>
         <div class="col-md-6">
           <input type="text" name="nationality" placeholder="nationality*">
         </div>
-      </div>
-      <div class="row">
         <div class="col-md-6">
           <input type="text" name="height" placeholder="height*">
         </div>
         <div class="col-md-6">
           <input type="text" name="mesurements" placeholder="mesurements (E.G. 90 - 60 - 90)*">
         </div>
+
       </div>
       <div class="row">
-        <textarea name="" id="" cols="30" rows="10" class="col-md-12"></textarea>
+        <div class="col-md-12">
+          <textarea name="" id="" cols="30" rows="10" class="col-md-12" placeholder="few words about you*"></textarea>
+        </div>
       </div>
       <div class="row upload-photo">
-        <p class="col-md-12 title">Upload photo</p>
-        <input type="file" class="" name="photo1">
-        <input type="file" class="" name="photo2">
-        <input type="file" class="" name="photo3">
+          <p class="col-md-12 title subheading-main">Upload photo</p>
+          <div class="col-md-4">
+            <input type="file" class="" name="photo1">
+          </div>
+          <div class="col-md-4">
+            <input type="file" class="" name="photo2">
+          </div>
+          <div class="col-md-4">
+            <input type="file" class="" name="photo3">
+          </div>
       </div>
       <div class="row submit">
-        <button type="submit">Submit</button>
+        <div class="col-md-12 text-right">
+          <button type="submit">Submit</button>
+        </div>
       </div>
     </form>
   </div>
