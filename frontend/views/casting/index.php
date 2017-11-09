e4ccce94f3adb5e33e449eaab43dc7345823f68f
sadsenpai <sad.senpai.2001@gmail.com>
2017-11-09 15:41:34 +0200

    bottom slider looks fine

--- frontend/views/casting/index.php
+++ frontend/views/casting/index.php
@@ -322,37 +322,109 @@
 </div>
 
 <div class="row">
-  <div class="bottom-items-slider">
+  <div class="col bottom-items-slider">
     <div class="slick-carousel">
-        <div class="slider-item">
-          <div class="item-box girl" style="background-image: url('img/cat.jpg')">
-            <div class="item-box__title">
-              <a href="#">Oksana</a>
-            </div>
-            <div class="item-box__info">
-              <span>Age: 28</span>
-              <span>Russia</span>
-            </div>
+
+      <div class="slider-item">
+        <div class="item-box girl" style="background-image: url('img/cat.jpg')">
+          <div class="item-box__title">
+            <a href="#">Oksana</a>
           </div>
-          <div class="item-box service" style="background-image: url('img/cat.jpg')">
-            <div class="item-box__title">
-              <a href="#story">Story</a>
-            </div>
-            <div class="item-box__info">
-              <a href="#story">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ipsam nam, itaque tempore consequatur facilis expedita et autem, iusto dolores maiores, similique praesentium beatae culpa enim? Magnam, porro voluptates? Praesentium.
-              </a>
-            </div>
-          </div>
-          <div class="item-box story" style="background-image: url('img/cat.jpg')">
-            <div class="item-box__title">
-              <a href="#">Service</a>
-            </div>
-            <div class="item-box__info">
-              <a href="#">Doggy Style</a>
-            </div>
+          <div class="item-box__info">
+            <span>Age: 28</span>
+            <span>Russia</span>
           </div>
         </div>
+      </div>
+      <div class="slider-item">
+        <div class="item-box service" style="background-image: url('img/cat.jpg')">
+          <div class="item-box__title">
+            <a href="#">Service</a>
+          </div>
+          <div class="item-box__info">
+            <a href="#">Doggy Style</a>
+          </div>
+        </div>
+      </div>
+      <div class="slider-item">
+        <div class="item-box story" style="background-image: url('img/cat.jpg')">
+          <div class="item-box__title">
+            <a href="#story">Story</a>
+          </div>
+          <div class="item-box__info">
+            <a href="#story">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ipsam nam, itaque tempore consequatur facilis expedita et autem, iusto dolores maiores, similique praesentium beatae culpa enim? Magnam, porro voluptates? Praesentium.
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="slider-item">
+        <div class="item-box girl" style="background-image: url('img/cat.jpg')">
+          <div class="item-box__title">
+            <a href="#">Oksana</a>
+          </div>
+          <div class="item-box__info">
+            <span>Age: 28</span>
+            <span>Russia</span>
+          </div>
+        </div>
+      </div>
+      <div class="slider-item">
+        <div class="item-box service" style="background-image: url('img/cat.jpg')">
+          <div class="item-box__title">
+            <a href="#">Service</a>
+          </div>
+          <div class="item-box__info">
+            <a href="#">Doggy Style</a>
+          </div>
+        </div>
+      </div>
+      <div class="slider-item">
+        <div class="item-box story" style="background-image: url('img/cat.jpg')">
+          <div class="item-box__title">
+            <a href="#story">Story</a>
+          </div>
+          <div class="item-box__info">
+            <a href="#story">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ipsam nam, itaque tempore consequatur facilis expedita et autem, iusto dolores maiores, similique praesentium beatae culpa enim? Magnam, porro voluptates? Praesentium.
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="slider-item">
+        <div class="item-box girl" style="background-image: url('img/cat.jpg')">
+          <div class="item-box__title">
+            <a href="#">Oksana</a>
+          </div>
+          <div class="item-box__info">
+            <span>Age: 28</span>
+            <span>Russia</span>
+          </div>
+        </div>
+      </div>
+      <div class="slider-item">
+        <div class="item-box service" style="background-image: url('img/cat.jpg')">
+          <div class="item-box__title">
+            <a href="#">Service</a>
+          </div>
+          <div class="item-box__info">
+            <a href="#">Doggy Style</a>
+          </div>
+        </div>
+      </div>
+      <div class="slider-item">
+        <div class="item-box story" style="background-image: url('img/cat.jpg')">
+          <div class="item-box__title">
+            <a href="#story">Story</a>
+          </div>
+          <div class="item-box__info">
+            <a href="#story">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ipsam nam, itaque tempore consequatur facilis expedita et autem, iusto dolores maiores, similique praesentium beatae culpa enim? Magnam, porro voluptates? Praesentium.
+            </a>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 </div>
