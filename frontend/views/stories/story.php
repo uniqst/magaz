4b8509d5d37f81518ffc9473908b8e72b2483576
xzACzx <zAC95zUA@gmail.com>
2017-11-13 20:00:32 +0200

    commit

--- frontend/views/stories/story.php
+++ frontend/views/stories/story.php
@@ -36,45 +36,10 @@ use yii\helpers\Url;
   <div class="col-lg-6">
     <div class="story-long">
       <div class="story__image">
-        <img src="http://via.placeholder.com/350x450" alt="Girl image">
+        <img src="/stori/<?= $model->img?>" width="350px" hight="450px" alt="Girl image">
       </div>
       <p>
         <?= $model->content?>
-      </p>
-      
-      <div class="story__image">
-        <img src="http://via.placeholder.com/350x450" alt="Girl image">
-      </div>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed amet rerum consequatur minima, labore quas sunt natus dolor, tempore deserunt nam itaque voluptatem saepe molestiae ut libero quos optio obcaecati!Itaque deleniti molestiae, vero perspiciatis debitis optio fugit saepe, tempore consectetur cum dignissimos blanditiis velit quam corporis voluptas laboriosam soluta a aliquid. Cum doloremque in libero sunt saepe laborum facilis!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum delectus fugiat deserunt hic facilis. Tenetur dolorum, voluptatibus deserunt quaerat, maxime sunt modi a ipsum pariatur facere ad aperiam provident praesentium.
-        Dicta rem alias amet fugiat temporibus voluptatem dolor eveniet vero assumenda officiis. Libero, voluptate vero repudiandae similique excepturi iste numquam facilis quos qui ex voluptas eveniet repellendus, corrupti perferendis esse!
-        Veniam neque sed, sapiente quasi iusto temporibus ad iste beatae perferendis eveniet obcaecati qui magni rerum atque corrupti distinctio culpa soluta recusandae provident repellendus maiores illum error, molestias velit. Quia!
-        Tenetur doloremque facere esse obcaecati culpa consequatur ratione laudantium commodi necessitatibus fugit! Delectus impedit earum, eos laudantium molestias omnis totam sint repellendus consectetur explicabo consequatur accusantium repellat. Maiores, itaque atque.
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem aspernatur saepe magnam aperiam consectetur facere ut totam quo qui itaque neque, deleniti similique sint maxime obcaecati, recusandae beatae deserunt. Magni.
-        Quo id, cumque, iusto, veniam tenetur odit perferendis quisquam natus tempore nam libero repellendus! Blanditiis hic impedit repellat sint ullam fugiat, optio ducimus pariatur, modi nostrum asperiores eligendi esse! Tempore?
-        Dicta officiis harum ullam ut earum debitis impedit! Quia maxime fugit, molestiae nesciunt consequuntur, enim at vel corrupti quod cupiditate sapiente voluptatibus quae soluta alias pariatur cum, impedit voluptas maiores.
-      </p>
-      <div class="story__image">
-        <img src="http://via.placeholder.com/350x450" alt="Girl image">
-      </div>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed amet rerum consequatur minima, labore quas sunt natus dolor, tempore deserunt nam itaque voluptatem saepe molestiae ut libero quos optio obcaecati!Itaque deleniti molestiae, vero perspiciatis debitis optio fugit saepe, tempore consectetur cum dignissimos blanditiis velit quam corporis voluptas laboriosam soluta a aliquid. Cum doloremque in libero sunt saepe laborum facilis!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum delectus fugiat deserunt hic facilis. Tenetur dolorum, voluptatibus deserunt quaerat, maxime sunt modi a ipsum pariatur facere ad aperiam provident praesentium.
-        Dicta rem alias amet fugiat temporibus voluptatem dolor eveniet vero assumenda officiis. Libero, voluptate vero repudiandae similique excepturi iste numquam facilis quos qui ex voluptas eveniet repellendus, corrupti perferendis esse!
-        Veniam neque sed, sapiente quasi iusto temporibus ad iste beatae perferendis eveniet obcaecati qui magni rerum atque corrupti distinctio culpa soluta recusandae provident repellendus maiores illum error, molestias velit. Quia!
-        Tenetur doloremque facere esse obcaecati culpa consequatur ratione laudantium commodi necessitatibus fugit! Delectus impedit earum, eos laudantium molestias omnis totam sint repellendus consectetur explicabo consequatur accusantium repellat. Maiores, itaque atque.
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem aspernatur saepe magnam aperiam consectetur facere ut totam quo qui itaque neque, deleniti similique sint maxime obcaecati, recusandae beatae deserunt. Magni.
-        Quo id, cumque, iusto, veniam tenetur odit perferendis quisquam natus tempore nam libero repellendus! Blanditiis hic impedit repellat sint ullam fugiat, optio ducimus pariatur, modi nostrum asperiores eligendi esse! Tempore?
-        Dicta officiis harum ullam ut earum debitis impedit! Quia maxime fugit, molestiae nesciunt consequuntur, enim at vel corrupti quod cupiditate sapiente voluptatibus quae soluta alias pariatur cum, impedit voluptas maiores.
       </p>
     </div>
   </div>
