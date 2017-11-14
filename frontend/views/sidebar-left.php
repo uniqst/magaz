35c02c773d2f2c746dafa0921e0fff8b8b1325fd
sadsenpai <sad.senpai.2001@gmail.com>
2017-11-14 15:00:16 +0200

    left sidebar for xl screen is done

--- frontend/views/sidebar-left.php
+++ frontend/views/sidebar-left.php
@@ -3,60 +3,62 @@
     <div class="page-name">
       Catalog
     </div>
+    <h2 class="sb-heading-main">
+      Escort cities
+    </h2>
     <div class="category-list-wraper">
-      <h2 class="sb-heading-main">
-        Escort cities
-      </h2>
       <ul class="category-list">
         <li class="category-list__item">
           <a href="">
-            Ankara escorts
+            ankara escorts
           </a>
         </li>
         <li class="category-list__item">
           <a href="">
-            Ankara escorts
+            bursa  escorts
           </a>
         </li>
         <li class="category-list__item">
           <a href="">
-            Ankara escorts
+            istanbul escorts
           </a>
         </li>
       </ul>
     </div>
+    <h2 class="sb-heading-main">
+      Escort menu
+    </h2>
     <div class="category-list-wraper">
-      <h2 class="sb-heading-main">
-        Escort menu
-      </h2>
       <ul class="category-list">
         <li class="category-list__item">
           <a href="">
-            Ankara escorts
+            new girls ankara
           </a>
         </li>
         <li class="category-list__item">
           <a href="">
-            Ankara escorts
+            russian escort ankara
           </a>
         </li>
         <li class="category-list__item">
           <a href="">
-            Ankara escorts
+            ankara escort agency
           </a>
         </li>
       </ul>
     </div>
-    <div class="category-list-wraper">
-      <h2 class="sb-heading-main opened">
+    <h2 class="sb-heading-main sb-heading-main__link">
+      <a href="#list-filters" class="sb-heading-main__link" data-toggle="collapse" aria-expanded="true">
         Filters
-      </h2>
-      <h3 class="category-heading opened">
-        <a href="#">
+      </a>
+    </h2>
+    <div class="category-list-wraper collapse show" id="list-filters">
+      <h3 class="category-heading">
+        <a href="#list-categories" data-toggle="collapse" aria-expanded="true">
           Categories
         </a>
       </h3>
-      <ul class="category-list">
+      <ul class="category-list category-list--border collapse show" id="list-categories">
         <li class="category-list__item">
           <a href="#">
             All
@@ -80,6 +82,255 @@
         <li class="category-list__item">
           <a href="#">
             Vip escort
+          </a>
+        </li>
+      </ul>
+
+      <h3 class="category-heading">
+        <a href="#list-age" data-toggle="collapse" aria-expanded="true">
+          age
+        </a>
+      </h3>
+      <ul class="category-list category-list--border collapse show" id="list-age">
+        <li class="category-list__item">
+          <a href="#">
+            All
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+            18-20
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+            Russian escort
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+            21-23
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+            24+
+          </a>
+        </li>
+      </ul>
+
+      <h3 class="category-heading">
+        <a href="#list-nationality" data-toggle="collapse" aria-expanded="true">
+          nationality
+        </a>
+      </h3>
+      <ul class="category-list category-list--border collapse show" id="list-nationality">
+        <li class="category-list__item">
+          <a href="#">
+            All
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+            russian
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+            ukrainian
+          </a>
+        </li>
+      </ul>
+      <h3 class="category-heading">
+        <a href="#list-brsize" data-toggle="collapse" aria-expanded="true">
+        breast size
+        </a>
+      </h3>
+      <ul class="category-list category-list--border collapse show" id="list-brsize">
+        <li class="category-list__item">
+          <a href="#">
+            All
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          all
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          0-1
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+            2-3
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+            4-5
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+            6-12
+          </a>
+        </li>
+      </ul>
+      <h3 class="category-heading">
+        <a href="#list-height" data-toggle="collapse" aria-expanded="true">
+        height (in sm)
+        </a>
+      </h3>
+      <ul class="category-list category-list--border collapse show" id="list-height">
+        <li class="category-list__item">
+          <a href="#">
+          all
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          less 160
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          160-170
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          170-180
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          more 180
+          </a>
+        </li>
+      </ul>
+      <h3 class="category-heading">
+        <a href="#list-weight" data-toggle="collapse" aria-expanded="true">
+        weight (in kg )
+        </a>
+      </h3>
+      <ul class="category-list category-list--border collapse show" id="list-weight">
+        <li class="category-list__item">
+          <a href="#">
+            All
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+            less 55
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+            55-60
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+            60-65
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+            more 65
+          </a>
+        </li>
+      </ul>
+      <h3 class="category-heading">
+        <a href="#list-services" data-toggle="collapse" aria-expanded="true">
+          services
+        </a>
+      </h3>
+      <ul class="category-list category-list--border collapse show" id="list-services">
+        <li class="category-list__item">
+          <a href="#">
+            All
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          69
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          bj (blowjob) 
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          cim (come in mouth)
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          cob (cum on body)
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          couples
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          dfk (deep french kiss) doggie style
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          extraball (have sex many times)
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          fetish
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          foreplay
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          gfe (girlfriend experience)
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          golden shower
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          hj (hand job)
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          ld (lap dance)
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          massage (amateur)
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          owo (oral without condom)
+          </a>
+        </li>
+        <li class="category-list__item">
+          <a href="#">
+          strapon
           </a>
         </li>
       </ul>
