1d1674ea7af5a5988be4924ed3088cb98a67d8b4
Galifax <uniqstudioteam@gmail.com>
2017-11-16 17:01:40 +0200

    rand sidebar

--- frontend/components/SidebarRight.php
+++ frontend/components/SidebarRight.php
@@ -2,10 +2,16 @@
 namespace frontend\components;
 
 use yii\base\Widget;
+use frontend\models\Profile;
+use frontend\models\Stories;
 
 
 Class SidebarRight extends Widget{
     public function run(){
-        return $this->render('sidebar-right');
+        $profile = Profile::find()->limit(3)->with('image')->all();
+        $stories = Stories::find()->limit(3)->all();
+        $model = array_merge($profile, $stories);
+        shuffle($model);
+        return $this->render('sidebar-right', compact('model'));
     }
 }
--- frontend/components/views/sidebar-right.php
+++ frontend/components/views/sidebar-right.php
@@ -1,38 +1,72 @@
 <div class="main-sidebar-right">
-  <div class="page-name">
-    Intresting
-  </div>
-    <div class="slider-item">
-      <div class="item-box girl" style="background-image: url('../img/cat.jpg')">
-        <div class="item-box__title">
-          <a href="#">Oksana</a>
-        </div>
-        <div class="item-box__info">
-          <span>Age: 28</span>
-          <span>Russia</span>
-        </div>
-      </div>
+    <div class="page-name">
+        Intresting
     </div>
-    <div class="slider-item">
-      <div class="item-box service" style="background-image: url('../img/cat.jpg')">
-        <div class="item-box__title">
-          <a href="#">Service</a>
-        </div>
-        <div class="item-box__info">
-          <a href="#">Doggy Style</a>
-        </div>
-      </div>
-    </div>
-    <div class="slider-item">
-      <div class="item-box story" style="background-image: url('../img/cat.jpg')">
-        <div class="item-box__title">
-          <a href="#story">Story</a>
-        </div>
-        <div class="item-box__info">
-          <a href="#story">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ipsam nam, itaque tempore consequatur facilis expedita et autem, iusto dolores maiores, similique praesentium beatae culpa enim? Magnam, porro voluptates? Praesentium.
-          </a>
-        </div>
-      </div>
-    </div>
+    <?php foreach($model as $rand):?>
+        <?php if(isset($rand->image)):?>
+            <div class="slider-item">
+                <div class="item-box girl" style="background-image: url('/photo/<?=$rand->image->src?>')">
+                    <div class="item-box__title">
+                        <a href="#"><?=$rand->name?></a>
+                    </div>
+                    <div class="item-box__info">
+                        <span>Age: <?=$rand->age?></span>
+                        <span>R<?=$rand->nationality?></span>
+                    </div>
+                </div>
+            </div>
+        <?php elseif(isset($rand->img)):?>
+            <div class="slider-item">
+                <div class="item-box story" style="background-image: url('/story/<?=$rand->img?>')">
+                    <div class="item-box__title">
+                        <a href="#story">Story</a>
+                    </div>
+                    <div class="item-box__info">
+                        <a href="#story">
+                            <?=$rand->content?>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        <?php endif;?>
+    <?php endforeach;?>
 </div>
+
+<!--<div class="main-sidebar-right">-->
+<!--  <div class="page-name">-->
+<!--    Intresting-->
+<!--  </div>-->
+<!--    <div class="slider-item">-->
+<!--      <div class="item-box girl" style="background-image: url('../img/cat.jpg')">-->
+<!--        <div class="item-box__title">-->
+<!--          <a href="#">Oksana</a>-->
+<!--        </div>-->
+<!--        <div class="item-box__info">-->
+<!--          <span>Age: 28</span>-->
+<!--          <span>Russia</span>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <div class="slider-item">-->
+<!--      <div class="item-box service" style="background-image: url('../img/cat.jpg')">-->
+<!--        <div class="item-box__title">-->
+<!--          <a href="#">Service</a>-->
+<!--        </div>-->
+<!--        <div class="item-box__info">-->
+<!--          <a href="#">Doggy Style</a>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <div class="slider-item">-->
+<!--      <div class="item-box story" style="background-image: url('../img/cat.jpg')">-->
+<!--        <div class="item-box__title">-->
+<!--          <a href="#story">Story</a>-->
+<!--        </div>-->
+<!--        <div class="item-box__info">-->
+<!--          <a href="#story">-->
+<!--            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ipsam nam, itaque tempore consequatur facilis expedita et autem, iusto dolores maiores, similique praesentium beatae culpa enim? Magnam, porro voluptates? Praesentium.-->
+<!--          </a>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--</div>-->
