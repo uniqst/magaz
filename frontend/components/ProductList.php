3f8f19d19bba085ca09f19efa588f5373cd89d83
xzACzx <zAC95zUA@gmail.com>
2017-11-17 23:49:21 +0200

    pagination

--- frontend/components/ProductList.php
+++ frontend/components/ProductList.php
@@ -1,13 +1,24 @@
 <?php
 namespace frontend\components;
 
+use yii\data\Pagination;
 use yii\base\Widget;
 
 Class ProductList extends Widget{
     public $model;
+
     public function run(){
+    	 $countQuery = clone $this->model;
+    // подключаем класс Pagination, выводим по 10 пунктов на страницу
+   			 $pages = new Pagination(['totalCount' => $countQuery->count(), 'pageSize' => 2]);
+    // приводим параметры в ссылке к ЧПУ
+    		$pages->pageSizeParam = false;
+    		$models = $this->model->offset($pages->offset)
+        		->limit($pages->limit)
+        		->all();
         return $this->render('product-list', [
-            'model' => $this->model
+            'model' => $models,
+              'pages' => $pages
         ]);
     }
 }
