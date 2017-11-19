27caeb98fdda08c0ade8a611ea85026ebe1a48eb
sadsenpai <sad.senpai.2001@gmail.com>
2017-11-19 19:34:50 +0200

    create all files

---
+++ frontend/views/one-girl.php
@@ -0,0 +1,64 @@
+<div class="row mx-0">
+    <!-- <div class="product-card"> -->
+
+    <div class="col-12">
+        <h1 class="page-name">girl_name</h1>
+        <a href="#" class="order">
+          Order now
+        </a>
+    </div>
+
+    <div class="col-12 next-prev-nav">
+      <div class="prev">
+        <button>
+          prev
+        </button>
+      </div>
+      <div class="next">
+        <button>
+          next
+        </button>
+      </div>
+    </div>
+
+    <div class="col-xl-7">
+      <div class="photo-block">
+        <div class="photo-block__photo" style="background-image: url('http://placehold.it/700x900?text=Image 1')">
+          <!-- <img src="http://placehold.it/400x400" alt="" class=""> -->
+        </div>
+        <div class="photo-block__list flex-column">
+          <img src="http://placehold.it/600x900?text=Image 1" alt="" class="photo-tn">
+          <img src="http://placehold.it/600x900?text=Image 2" alt="" class="photo-tn">
+          <img src="http://placehold.it/600x900?text=Image 3" alt="" class="photo-tn">
+          <img src="http://placehold.it/600x900?text=Image 4" alt="" class="photo-tn">
+        </div>
+      </div>
+      <h3 class="headline">Price</h3>
+      Girl info and photo
+    </div>
+
+    <div class="col-xl-5">
+      <div class="card__info-list">
+        <ul>
+          <li class="info-list__item">
+            <span class="property">Age</span>
+            <span class="value">18</span>
+          </li>
+          <li class="info-list__item">
+            <span class="property">Age</span>
+            <span class="value">18</span>
+          </li>
+          <li class="info-list__item">
+            <span class="property">Age</span>
+            <span class="value">18</span>
+          </li>
+        </ul>
+      </div>
+      <h3 class="headline">Categories and Services</h3>
+    </div>
+    <div class="col-12">
+      <h3 class="headline">About girl_name</h3>
+
+    </div>
+
+  </div>
