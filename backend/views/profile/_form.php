f6192a17a5e85bb83f5e987256a83503fc1c2968
Galifax <uniqstudioteam@gmail.com>
2017-11-22 14:17:03 +0200

    fix admin

--- backend/views/profile/_form.php
+++ backend/views/profile/_form.php
@@ -22,41 +22,68 @@ $category = Category::find()->where(['parent_id' => 0])->with('category')->all()
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'price_eur')->textInput(['maxlength' => true]) ?>
-
+    <div class="row">
+        <div class="col-md-2">
     <?= $form->field($model, 'price_try')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'price_usd')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'price_eur_two')->textInput(['maxlength' => true]) ?>
-
+        </div>
+        <div class="col-md-2">
     <?= $form->field($model, 'price_try_two')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'price_usd_two')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'price_eur_three')->textInput(['maxlength' => true]) ?>
-
+        </div>
+        <div class="col-md-2">
     <?= $form->field($model, 'price_try_three')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'price_usd_three')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'price_eur_four')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'price_try_four')->textInput(['maxlength' => true]) ?>
-
+        </div>
+        <div class="col-md-2">
     <?= $form->field($model, 'price_usd_four')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'price_eur_night')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'price_try_night')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'price_usd_night')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'taxi_eur')->textInput(['maxlength' => true]) ?>
-
+        </div>
+        <div class="col-md-2">
     <?= $form->field($model, 'taxi_try')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-2">
+        <?= $form->field($model, 'price_try_night')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
 
-    <?= $form->field($model, 'taxi_usd')->textInput(['maxlength' => true]) ?>
+    <div class="row">
+        <div class="col-md-2">
+            <?= $form->field($model, 'price_usd')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-2">
+            <?= $form->field($model, 'price_usd_two')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-2">
+            <?= $form->field($model, 'price_usd_three')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-2">
+            <?= $form->field($model, 'price_usd_four')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-2">
+            <?= $form->field($model, 'taxi_usd')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-2">
+            <?= $form->field($model, 'price_usd_night')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-2">
+            <?= $form->field($model, 'price_eur')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-2">
+            <?= $form->field($model, 'price_eur_two')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-2">
+            <?= $form->field($model, 'price_eur_three')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-2">
+            <?= $form->field($model, 'price_eur_four')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-2">
+            <?= $form->field($model, 'taxi_eur')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-2">
+            <?= $form->field($model, 'price_eur_night')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
 
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
