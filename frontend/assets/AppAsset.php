ed00609f644131346eaa685e07f74af3cb701410
sadsenpai <sad.senpai.2001@gmail.com>
2017-11-24 18:14:24 +0200

    add new slider library

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -25,6 +25,7 @@ class AppAsset extends AssetBundle
     ];
     public $js = [
         'https://code.jquery.com/jquery-3.2.1.min.js',
+        'js/libs/jquery.featureCarousel.min.js',
         'https://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js',
         'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js',
