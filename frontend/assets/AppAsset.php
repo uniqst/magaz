6a33063d2e57a4b6c90436af16218af3bd27b92f
sadsenpai <sad.senpai.2001@gmail.com>
2017-11-24 21:00:18 +0200

    slider looks fuckig weird, but it`s done

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -24,8 +24,10 @@ class AppAsset extends AssetBundle
         'css/styles.css',
     ];
     public $js = [
+        // 'http://code.jquery.com/jquery-1.9.1.min.js',
         'https://code.jquery.com/jquery-3.2.1.min.js',
-        'js/libs/jquery.featureCarousel.min.js',
+        // 'js/libs/jquery.waterwheelCarousel.min.js',
+        'js/libs/jquery.waterwheelCarousel.js',
         'https://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js',
         'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js',
