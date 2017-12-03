50ea598e61c704f0a0790f0d3a5076ef394d5d0f
Galifax <uniqstudioteam@gmail.com>
2017-12-03 21:44:23 +0200

    sidebar

--- frontend/components/views/sidebar-left.php
+++ frontend/components/views/sidebar-left.php
@@ -20,10 +20,12 @@ use yii\helpers\Url;
         <div class="category-list-wraper collapse show" id="list-filtersasdas">
             <h2 class="sb-heading-main sb-heading-main__link">
               <?= Yii::t('app', 'City')?>
+           
           </h2>
-          <ul>
+          <ul class="category-list category-list--border collapse show">
+
             <?php foreach($city as $val):?>
-            <li>
+            <li class="category-list__item">
                 <a href="<?= $val->href?>"><?= Yii::t('app', $val->name)?></a>
             </li>
           <?php endforeach;?>
@@ -34,26 +36,26 @@ use yii\helpers\Url;
               <?= Yii::t('app', 'Filters')?>
           </h2>
 
-        <div class="category-list-wraper collapse show" id="list-filtersasdas">
-          <?php foreach($model as $filter):?>
-            <a href="#list-categories<?=$filter->id?>" style="color: white; margin-left: 30px; padding-bottom: 10px;" class="sb-heading-main__link" data-toggle="collapse" aria-expanded="true">
-              <?= Yii::t('app', $filter->name)?>
-            </a>
-          <ul class="category-list category-list--border collapse show" id="list-categories<?=$filter->id?>">
-            <li class="category-list__item">
-              <a href="#" oclick="return false"class="inputAll">
-                <?=Yii::t('app', 'All');?>
-              </a>
-            </li>
-            <?php foreach($filter->category as $val):?>
-            <li class="category-list__item">
-                <input style="display: none;" type="checkbox" class="check<?=$val->id?> inputCheck" <?php if($_GET['value'][$val->id] == $val->id) echo 'checked';?>  onclick="this.form.submit()" id="value<?=$val->id?>" name="value[<?= $val->id?>]" value="<?=$val->id?>">
-                <label class="labelcategory" for="value<?=$val->id?>" <?php if($_GET['value'][$val->id] == $val->id) echo "style='color: red;'";?>><?=$val->name?></label>
-            </li>
-          <?php endforeach;?>
-          </ul>
-          <?php endforeach;?>
-        </div>
+          <div class="category-list-wraper collapse show" id="list-filtersasdas">
+              <?php foreach($model as $filter):?>
+                  <a href="#list-categories<?=$filter->id?>" style="color: white; margin-left: 20px; font-size: 14px; padding-bottom: 10px;" class="sb-heading-main__link" data-toggle="collapse" aria-expanded="true">
+                      <?= Yii::t('app', $filter->name)?>
+                  </a>
+                  <ul class="category-list category-list--border collapse show" id="list-categories<?=$filter->id?>">
+                      <li class="category-list__item">
+                          <a href="#" oclick="return false"class="inputAll">
+                              <?=Yii::t('app', 'All');?>
+                          </a>
+                      </li>
+                      <?php foreach($filter->category as $val):?>
+                          <li class="category-list__item">
+                              <input style="display: none;" type="checkbox" class="check<?=$val->id?> inputCheck" <?php if($_GET['value'][$val->id] == $val->id) echo 'checked';?>  onclick="this.form.submit()" id="value<?=$val->id?>" name="value[<?= $val->id?>]" value="<?=$val->id?>">
+                              <label class="labelcategory" for="value<?=$val->id?>" <?php if($_GET['value'][$val->id] == $val->id) echo "style='color: red;'";?>><?=$val->name?></label>
+                          </li>
+                      <?php endforeach;?>
+                  </ul>
+              <?php endforeach;?>
+          </div>
       </div>
     </form>
 </div>
