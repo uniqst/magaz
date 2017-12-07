d2d60a1439f8a39c3e3a18ab1dc4c10ee46a0137
xzACzx <zAC95zUA@gmail.com>
2017-12-07 03:09:40 +0200

    FixHow

--- frontend/components/Product.php
+++ frontend/components/Product.php
@@ -2,16 +2,17 @@
 namespace frontend\components;
 use yii\base\Widget;
 use frontend\models\Profile;
+use frontend\models\Contacts;
 use yii\db\Expression;
 
 Class Product extends Widget{
 
 	public function run(){
-
+		 $contact = Contacts::find()->one();
 		$model = Profile::find()->limit(2)->orderBy(new Expression('rand()'))->with(['photo' => function(\yii\db\ActiveQuery $query){
 			$query->limit(2);
 		}])->all();
-		return $this->render('product', compact('model'));
+		return $this->render('product', compact('model', 'contact'));
 
 	}
 
--- frontend/components/views/product.php
+++ frontend/components/views/product.php
@@ -6,7 +6,7 @@ use yii\helpers\Url;
 
       <div class="girl-info__title">
         <a href="<?= Url::to(['/girls/girl', 'id' => $product->id, 'name' => $product->name]) ?>">
-          <?= Yii::t('app', $profile->name_description)?><?= $product->name?>
+          <?= Yii::t('app', $product->name_description)?> <?= $product->name?>
         </a>
       </div>
 
@@ -28,36 +28,68 @@ use yii\helpers\Url;
               </div>
             </div>
             <div class="card__description">
-             <?= Yii::t('app', $profile->$product->about_myself)?>
+             <?= Yii::t('app', $product->about_myself)?>
             </div>
           </div>
           <div class="col-md-4">
             <div class="card__info-list">
               <ul>
                 <li class="info-list__item">
-                  <span class="property"><?=Yii::t('app','Age');?></span>
-                  <span class="value"><?= $product->age?></span>
-                </li>
-                <li class="info-list__item">
-                  <span class="property"><?= Yii::t('app', 'Nationality')?></span>
-                  <span class="value"><?= $product->nationality?></span>
-                </li>
-                <li class="info-list__item">
-                  <span class="property"><?= Yii::t('app', 'Height')?></span>
-                  <span class="value"><?= $product->height?></span>
-                </li>
-                <li class="info-list__item">
-                  <span class="property"><?= Yii::t('app', 'Weight')?></span>
-                  <span class="value"><?= $product->weight?></span>
-                </li>
-                <li class="info-list__item">
-                  <span class="property"><?= Yii::t('app', 'Options')?></span>
-                  <span class="value"><?= $product->mesurements?></span>
-                </li>
-                <li class="info-list__item">
-                  <span class="property"><?= Yii::t('app', 'Contact')?></span>
-                  <span class="value"><?= $product->phone?></span>
-                </li>
+                            <span class="property"><?=Yii::t('app', 'AGE')?></span>
+                            <span class="value"><?= $product->age?></span>
+                        </li>
+                        <li class="info-list__item">
+                            <span class="property"><?=Yii::t('app', 'NATIONALITY')?></span>
+                            <span class="value"><?= $product->nationality?></span>
+                        </li>
+                        <li class="info-list__item">
+                            <span class="property"><?=Yii::t('app', 'HEIGHT')?></span>
+                            <span class="value"><?= $product->height?></span>
+                        </li>
+                        <li class="info-list__item">
+                            <span class="property"><?=Yii::t('app', 'WEIGHT')?></span>
+                            <span class="value"><?= $product->weight?></span>
+                        </li>
+                        <li class="info-list__item">
+                            <span class="property"><?=Yii::t('app', 'CHEST')?></span>
+                            <span class="value"><?= $product->chest?></span>
+                        </li>
+                        <li class="info-list__item">
+                            <span class="property"><?=Yii::t('app', 'OPTIONS')?></span>
+                            <span class="value"><?= $product->mesurements?></span>
+                        </li>
+                        <li class="info-list__item">
+                            <span class="property"><?=Yii::t('app', 'HAIR COLOR')?></span>
+                            <span class="value"><?= $product->hair?></span>
+                        </li>
+                        <li class="info-list__item">
+                            <span class="property"><?=Yii::t('app', 'EYE COLOR')?></span>
+                            <span class="value"><?= $product->eye?></span>
+                        </li>
+                        <li class="info-list__item">
+                            <span class="property"><?=Yii::t('app', 'LANGUAGES')?></span>
+                            <span class="value"><?= $product->language?></span>
+                        </li>
+                        <li class="info-list__item">
+                            <span class="property"><?=Yii::t('app', 'CONTACT')?></span>
+                            <span class="value"><?= $contact->phone?></span>
+                        </li>
+                        <li class="info-list__item">
+                            <span class="property"><?=Yii::t('app', 'EMAIL')?></span>
+                            <span class="value"><?= $contact->email?></span>
+                        </li>
+                        <li class="info-list__item">
+                            <span class="property"><?=Yii::t('app', 'CITY')?></span>
+                            <span class="value"><?= $product->city?></span>
+                        </li>
+                        <li class="info-list__item">
+                            <span class="property"><?=Yii::t('app', 'WORKING HOURS')?></span>
+                            <span class="value"><?= $product->working_hours?></span>
+                        </li>
+                        <li class="info-list__item">
+                            <span class="property"><?=Yii::t('app', 'REGISTRATE')?></span>
+                            <span class="value"><?= date('d-m-Y', time($product->date))?></span>
+                        </li>
               </ul>
             </div>
           </div>
