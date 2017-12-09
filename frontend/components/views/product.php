4e2785a1e74e23a7f6c4388578d075468dd33d65
galifax <uniqstudioteam@gmail.com>
2017-12-09 16:26:18 +0200

    link

--- frontend/components/views/product.php
+++ frontend/components/views/product.php
@@ -4,28 +4,28 @@ use yii\helpers\Url;
 <?php foreach($model as $product):?>
 <div class="girl-info">
 
-      <div class="girl-info__title">
+    <div class="girl-info__title">
+        <p><?= Yii::t('app', $product->name_description)?></p>
         <a href="<?= Url::to(['/girls/girl', 'id' => $product->id, 'name' => $product->name]) ?>">
-          <?= Yii::t('app', $product->name_description)?> <?= $product->name?>
+            <?= Yii::t('app',$product->name)?>
         </a>
-      </div>
 
-      <div class="girl-info__card container-fluid">
+    </div>
+
+
+    <div class="girl-info__card container-fluid">
         <div class="row">
           <div class="col-md-8">
             <div class="card__photo-section container-fluid">
               <div class="row">
-                <!-- <div class="photo" style="background-image: url(img/cat.jpg)"></div>
-                <div class="photo" style="background-image: url(img/cat.jpg)"></div> -->
+
                 <?php $photos = array_slice($product->photo, 0, 2);?>
                 <?php foreach($photos as $photo):?>
                 <div class="col">
                   <div class="photo" style="background-image: url(/photo/<?= $photo->src?>)"></div>
                 </div>
                 <?php endforeach;?>
-                <!-- <div class="d-none col">
-                  <div class="photo" style="background-image: url(img/cat.jpg)"></div>
-                </div> -->
+
               </div>
             </div>
             <div class="card__description">
