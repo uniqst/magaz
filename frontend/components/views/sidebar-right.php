4950108db40c7496e011298914c0b3f5751ded4b
xzACzx <zAC95zUA@gmail.com>
2017-12-15 17:38:43 +0200

    text1

--- frontend/components/views/sidebar-right.php
+++ frontend/components/views/sidebar-right.php
@@ -8,9 +8,8 @@
         <?php if($rand->date == 'profile'):?>
      <div class="slider-item">
                 <div class="item-box girl" style="background-image: url('/photo/<?=$rand->image->src?>')">
-                    <a href="<?=Url::to(['/girls/girl', 'id' => $rand->id, 'name' => $rand->name])?>" class="items-block__link"></a>
                     <div class="item-box__title">
-                        <a href="<?=Url::to(['/girls/girl', 'id' => $rand->id, 'name' => $rand->name])?>"><?= Yii::t('app', $rand->name)?></a>
+                        <a href="<?=Url::to(['/girls/girl', 'id' => $rand->id, 'name' => $rand->name])?>" class="items-block__link"><?= Yii::t('app', $rand->name)?></a>
                     </div>
                     <div class="item-box__info">
                         <span>Age: <?= Yii::t('app', $rand->age)?></span>
