534a2ccc9f72866cab8457a7c635dcd914222b6a
sadsenpai <sad.senpai.2001@gmail.com>
2018-02-23 14:50:20 +0200

    verification page is done

--- frontend/views/verification/index.php
+++ frontend/views/verification/index.php
@@ -1,26 +1,58 @@
-<div class="bubbles"></div>
-<div class="wrap">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta name="csrf-param" content="_csrf-frontend">
+    <meta name="csrf-token" content="MtttrXrCDyjvrtQ3QGlEPpptrNOHoj_bre1zGLoKjcpcvwfkHpBpX6r_sAAPDnNmyzv1vbXmdIrUoUtB6Fu-oQ==">
+    <title>Bursa escort girls: the best services, excellent service, beautiful girls are ready for everything, the most beautiful girls of escort, escort models, escort date model, beautiful lady escort bursa, the best date of the night passionate model escort bursa
+</title>
+    <link rel="shortcut icon" type="image/png" href="favicon.png"><style></style>
+    <meta name="description" content="Bursa escort offers a huge selection of different options: a lot of services from the most beautiful girls who are ready to do much for the man&amp;rsquo;s pleasure You ">
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
+<link href="/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet">
+<link href="/css/font-awesome.min.css" rel="stylesheet">
+<link href="/css/site.css" rel="stylesheet">
+<link href="/css/styles.css" rel="stylesheet">
+</head>
+
+<div class="wrap">
     <div class="age-verify">
-        <div class="age-verify--text-block">
-            <p class="age-verify--main-text">
-                <a href="/">Allescortinankara.com</a>
-                , including all webpages, links and images , displays sexually explicit material. Only consenting adults are authorized beyond this page. If you are a minor (under the age of 18 years or 21 years where 18 isn't the legal age of majority), if sexually explicit material offends you or if it's illegal to view such material in your community, you MUST leave this site by clicking "LEAVE" below. By clicking "ENTER", you agree to the statement at the bottom of this page.
-            </p>
-        </div>
-        <div class="age-verify--text-block">
-            <p class="age-verify--headline">By entering Allescortinankara.com you agree that:</p>
-            <ul class="age-verify--list">
-                <li>you are an adult in your community and are at least 18 years old (21 in some countries)</li>
-                <li>you agree with terms and conditions</li>
-                <li>sexually explicit material is not deemed to be obscene or illegal in your community</li>
-                <li>you accept full responsibility for your own actions</li>
-            </ul>
-            <p class="age-verify--headline">You have read this agreement, understood it and agree to be bound by it.</p>
-        </div>
-        <div class="age-verify__buttons">
-            <a href="#" class="age-verify--enter">Enter</a>
-            <a href="#" class="age-verify--leave">Leave</a>
+        <header class="age-verify__header">
+            <a class="navbar-brand main-header__logo" href="/">
+                <div class="logo-image">
+                    <div class="logo-image__lips"></div>
+                </div>
+            </a>
+        </header>
+        <div class="age-verify--wrapper">
+            <div class="age-verify--text-block">
+                <p class="age-verify--main-text" style="padding-left: 30%;">
+                    <a href="/">Allescortinankara.com</a>
+                    , including all webpages, links and images , displays sexually explicit material. Only consenting adults are authorized beyond this page. If you are a minor (under the age of 18 years or 21 years where 18 isn't the legal age of majority), if sexually explicit material offends you or if it's illegal to view such material in your community, you MUST leave this site by clicking "LEAVE" below. By clicking "ENTER", you agree to the statement at the bottom of this page.
+                </p>
+            </div>
+            <div class="age-verify--text-block">
+                <p class="age-verify--headline">By entering Allescortinankara.com you agree that:</p>
+                <ul class="age-verify--list">
+                    <li>you are an adult in your community and are at least 18 years old (21 in some countries)</li>
+                    <li>you agree with terms and conditions</li>
+                    <li>sexually explicit material is not deemed to be obscene or illegal in your community</li>
+                    <li>you accept full responsibility for your own actions</li>
+                </ul>
+                <p class="age-verify--headline">You have read this agreement, understood it and agree to be bound by it.</p>
+            </div>
+            <div class="age-verify__buttons">
+                <a href="#" class="age-verify--enter">
+                    <span>Enter</span>
+                </a>
+                <a href="#" class="age-verify--leave">
+                    <span>Leave</span>
+                </a>
+            </div>
         </div>
     </div>
 </div>
