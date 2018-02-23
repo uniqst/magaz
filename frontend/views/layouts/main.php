1aaaca12e15f563889745ba4766617bbcdd4fb8d
sadsenpai <sad.senpai.2001@gmail.com>
2018-02-23 11:39:03 +0200

    save

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -31,36 +31,11 @@ AppAsset::register($this);
 </head>
 <body>
 <?php $this->beginBody() ?>
-<div class="wrap">
-
-    <div class="age-verify">
-        <div class="age-verify--text-block">
-            <p class="age-verify--main-text">
-                <a href="/">Allescortinankara.com</a>
-                , including all webpages, links and images , displays sexually explicit material. Only consenting adults are authorized beyond this page. If you are a minor (under the age of 18 years or 21 years where 18 isn't the legal age of majority), if sexually explicit material offends you or if it's illegal to view such material in your community, you MUST leave this site by clicking "LEAVE" below. By clicking "ENTER", you agree to the statement at the bottom of this page.
-            </p>
-        </div>
-        <div class="age-verify--text-block">
-            <p class="age-verify--headline">By entering Allescortinankara.com you agree that:</p>
-            <ul class="age-verify--list">
-                <li>you are an adult in your community and are at least 18 years old (21 in some countries)</li>
-                <li>you agree with terms and conditions</li>
-                <li>sexually explicit material is not deemed to be obscene or illegal in your community</li>
-                <li>you accept full responsibility for your own actions</li>
-            </ul>
-            <p class="age-verify--headline">You have read this agreement, understood it and agree to be bound by it.</p>
-        </div>
-        <div class="age-verify__buttons">
-            <a href="#" class="age-verify--enter">Enter</a>
-            <a href="#" class="age-verify--leave">Leave</a>
-        </div>
-    </div>
-</div>
 
 
 <div class="bubbles"></div>
 
-<div class="wrap" style="display: none">
+<div class="wrap">
 
     <nav class="navbar navbar-expand-lg main-header">
 
