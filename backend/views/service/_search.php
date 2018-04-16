ee23e3dca1a44c41001f502189d0592bd451ce75
uniqst <uniqstudioteam@gmail.com>
2018-04-16 15:38:55 +0300

    12312

--- backend/views/service/_search.php
+++ backend/views/service/_search.php
@@ -22,8 +22,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'name') ?>
 
-    <?= $form->field($model, 'description') ?>
-
     <?= $form->field($model, 'content') ?>
 
     <?= $form->field($model, 'img') ?>
